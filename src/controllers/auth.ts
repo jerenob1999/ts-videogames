b7352cf0279930a145faa5e6aaeb32208986fd7e
Jere <jere_newells@hotmail.com>
2023-05-31 09:38:37 -0300

    feature(/src/controllers/auth.ts && /src/services): added loggin function and inserted encrypted to auth

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,18 +1,26 @@
 import { Response, Request } from "express";
-import { registerNewUser } from "../services/auth";
+import { loginUser, registerNewUser } from "../services/auth";
 
 const registerController = async (req: Request, res: Response) => {
-    try {
-        const response = req.body
-        const responseUser = await registerNewUser(response);
-        res.send(responseUser)       
-    } catch (error:any) {
-        console.log(req.body)
-        console.log(error.message)
-        res.send("error")
-    }
+  try {
+    const response = req.body;
+    const responseUser = await registerNewUser(response);
+    res.send(responseUser);
+  } catch (error: any) {
+    console.log(req.body);
+    console.log(error.message);
+    res.send("error");
+  }
 };
 
-const logginController = async (_req: Request, _res: Response) => {};
+const logginController = async ({ body }: Request, res: Response) => {
+  try {
+    const { email, password } = body;
+    const responseUser = await loginUser({ email, password });
+    res.send(responseUser);
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
 
 export { registerController, logginController };
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,19 +1,29 @@
+import { Auth } from "../interfaces/auth.interface";
 import { User } from "../interfaces/user.interface";
 import UserModel from "../models/user";
+import { encrypt, verified } from "../utils/password.handle";
 
 const registerNewUser = async (body: User) => {
-  const {password,name,birthdate,email} = body
-  const check = await UserModel.findOne({email});
-  if (check) return "USER_ALREADY_EXIST"
-    const registerNewUser = await UserModel.create({
-        email,
-        password,
-        name,
-        birthdate,
-    })
+  const { password, name, birthdate, email } = body;
+  const check = await UserModel.findOne({ email });
+  if (check) return "USER_ALREADY_EXIST";
+  const hashPassword = await encrypt(password);
+  const registerNewUser = await UserModel.create({
+    email,
+    password: hashPassword,
+    name,
+    birthdate,
+  });
   return registerNewUser;
 };
 
-const loginUser = async () => {};
+const loginUser = async ({ email, password }: Auth) => {
+  const check = await UserModel.findOne({ email });
+  if (!check) return "USER_NOT_FOUND";
+  const encryptedPassword = check.password;
+  const verifyPassword = await verified(password, encryptedPassword);
+  if (!verifyPassword) return "INCORRECT_PASSWORD";
+  return verifyPassword;
+};
 
 export { registerNewUser, loginUser };
